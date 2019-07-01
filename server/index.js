947ef7264cf12a2b76850e65cd98f7c9d0cb9b95
robert-hadleyjones <robert.hadleyjones@gamesys.co.uk>
2019-07-01 10:39:41 +0100

    make consumer friendly

--- server/index.js
+++ server/index.js
@@ -1,44 +1,44 @@
-const express = require("express");
-const bodyParser = require("body-parser");
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const compare = require("../commands/compare");
-const removeStaticFolderPath = require("../utils/removeStaticFolderPath");
-const addStaticFolderPath = require("../utils/addStaticFolderPath");
-const updateImages = require("../utils/updateImages");
+const compare = require('../commands/compare');
+const removeStaticFolderPath = require('../utils/removeStaticFolderPath');
+const addStaticFolderPath = require('../utils/addStaticFolderPath');
+const updateImages = require('../utils/updateImages');
 
-const config = require("../imgCompare.config.js");
+const config = require(process.cwd() + '/imgCompare.config.js');
 
 const app = express();
 const port = 1800;
 
 exports.default = () => {
-	app.use(express.static(process.cwd() + "/dist"));
-	app.use(express.static(process.cwd() + config.directory));
+  app.use(express.static(process.cwd() + '/node_modules/imgcompare/dist'));
+  app.use(express.static(process.cwd() + config.directory));
 
-	app.use(
-		bodyParser.urlencoded({
-			extended: true
-		})
-	);
+  app.use(
+    bodyParser.urlencoded({
+      extended: true
+    })
+  );
 
-	app.use(bodyParser.json());
+  app.use(bodyParser.json());
 
-	app.set("views", process.cwd() + "/views");
+  app.set('views', process.cwd() + '/node_modules/imgcompare/views');
 
-	app.set("view engine", "ejs");
+  app.set('view engine', 'ejs');
 
-	app.get("/", (req, res) => {
-		compare().then(mismatchImages => {
-			const data = JSON.stringify(removeStaticFolderPath(mismatchImages)) || [];
-			res.render("index", {data});
-		});
-	});
+  app.get('/', (req, res) => {
+    compare().then(mismatchImages => {
+      const data = JSON.stringify(removeStaticFolderPath(mismatchImages)) || [];
+      res.render('index', { data });
+    });
+  });
 
-	app.put("/update-files", (req, res) => {
-		const imagesToUpdate = addStaticFolderPath(req.body.files);
-		updateImages(imagesToUpdate);
-		res.status(200).send("File Updated!");
-	});
+  app.put('/update-files', (req, res) => {
+    const imagesToUpdate = addStaticFolderPath(req.body.files);
+    updateImages(imagesToUpdate);
+    res.status(200).send('File Updated!');
+  });
 
-	app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+  app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 };
